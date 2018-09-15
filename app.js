0b3eeaa6dc09a41e3c236f414985d06c52b390f3
Adrian Gheorghe <adrian.gh.m.18@gmail.com>
2018-09-16 01:25:25 +0300

    init app

--- app.js
+++ app.js
@@ -7,8 +7,15 @@ var expressValidator = require('express-validator');
 var flash = require('connect-flash');
 var session = require('express-session');
 var passport = require('passport');
-var LocalStrategy = require('passport-local'), Strategy;
+var LocalStrategy = require('passport-local').Strategy;
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/loginapp');
 var db = mongoose.connection;
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+// init app
+
+var app = express();
